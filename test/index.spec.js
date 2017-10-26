df71bd9fe7f9249ee1221352867425c1a4a328c2
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-10-26 15:40:45 +0300

    Added HttpGetReader stub

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,4 +4,9 @@ require('chai')
 const dataplugHttp = require('../lib')
 
 describe('dataplug-http', () => {
+  it('has "HttpGetReader" class', () => {
+    dataplugHttp
+      .should.have.property('HttpGetReader')
+      .that.is.an('function')
+  })
 })
